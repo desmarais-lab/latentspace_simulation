25dea915bcc6089476a3d50a39924fd2ee48b8fd
Zachary M. Jones <zmj@zmjones.com>
2017-05-08 09:16:58 -0400

    change font size and title position

--- src/analysis_amen.R
+++ src/analysis_amen.R
@@ -23,8 +23,9 @@ ggplot(results, aes(eta, n, color = algorithm, linetype = algorithm)) +
   facet_grid(family + beta ~ nodes + latent_space, scales = "free", labeller = label_context) +
   labs(x = expression(eta), y = "Iterations Completed") +
   theme_bw() +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1), legend.position = "bottom")
-ggsave("iterations_amen.png", width = 12, height = 8)
+  theme(axis.text.x = element_text(angle = 90, hjust = 1), legend.position = "bottom",
+    plot.title = element_text(hjust = 0.5))
+ggsave("iterations_amen.png", width = 12, height = 8, scale = .75)
 
 ggplot(filter(results, beta == 1), aes(eta, loss,
   color = algorithm, linetype = algorithm)) +
@@ -33,8 +34,9 @@ ggplot(filter(results, beta == 1), aes(eta, loss,
   facet_grid(family ~ nodes + latent_space, scales = "free", labeller = label_context) +
   labs(x = expression(eta), y = "Mean Squared Error", title = "Generalization Error") +
   theme_bw() +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1), legend.position = "bottom")
-ggsave("generalization_amen.png", width = 12, height = 8)
+  theme(axis.text.x = element_text(angle = 90, hjust = 1), legend.position = "bottom",
+    plot.title = element_text(hjust = 0.5))
+ggsave("generalization_amen.png", width = 12, height = 8, scale = .75)
 
 ggplot(filter(results, beta == 1, latent_space == 1),
   aes(eta, bias, color = algorithm, linetype = algorithm)) +
@@ -44,8 +46,9 @@ ggplot(filter(results, beta == 1, latent_space == 1),
   geom_hline(aes(yintercept = 0), linetype = "dashed") +
   labs(x = expression(eta), y = "Bias", title = "Estimation Error w/ Latent Space") +
   theme_bw() +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1), legend.position = "bottom")
-ggsave("estimation_ls_amen.png", width = 10, height = 8)
+  theme(axis.text.x = element_text(angle = 90, hjust = 1), legend.position = "bottom",
+    plot.title = element_text(hjust = 0.5))
+ggsave("estimation_ls_amen.png", width = 10, height = 8, scale = .75)
 
 ggplot(filter(results, beta == 1, latent_space == 0),
   aes(eta, bias, color = algorithm, linetype = algorithm)) +
@@ -55,8 +58,9 @@ ggplot(filter(results, beta == 1, latent_space == 0),
   geom_hline(aes(yintercept = 0), linetype = "dashed") +
   labs(x = expression(eta), y = "Bias", title = "Estimation Error w/o Latent Space") +
   theme_bw() +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1), legend.position = "bottom")
-ggsave("estimation_nls_amen.png", width = 10, height = 8)
+  theme(axis.text.x = element_text(angle = 90, hjust = 1), legend.position = "bottom",
+    plot.title = element_text(hjust = 0.5))
+ggsave("estimation_nls_amen.png", width = 10, height = 8, scale = .75)
 
 ggplot(filter(results, type == 1),
   aes(eta, error, color = algorithm, linetype = algorithm)) +
@@ -66,8 +70,9 @@ ggplot(filter(results, type == 1),
   labs(x = expression(eta), y = expression(paste("Type 1 Error rate at ", alpha, " = 0.05")),
        title = "Type-1 Inferential Error") +
   theme_bw() +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1), legend.position = "bottom")
-ggsave("inference_type_1_amen.png", width = 12, height = 8)
+  theme(axis.text.x = element_text(angle = 90, hjust = 1), legend.position = "bottom",
+    plot.title = element_text(hjust = 0.5))
+ggsave("inference_type_1_amen.png", width = 12, height = 8, scale = .75)
 
 ggplot(filter(results, type == 2), aes(eta, error, color = algorithm, linetype = algorithm)) +
   scale_x_log10(breaks = c(1, 100, 1000000), labels = c("uniform correlation", "low correlation", "independence")) +
@@ -76,5 +81,6 @@ ggplot(filter(results, type == 2), aes(eta, error, color = algorithm, linetype =
   labs(x = expression(eta), y = expression(paste("Type 2 Error rate at ", alpha, " = 0.05")),
        title = "Type-2 Inferential Error") +
   theme_bw() +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1), legend.position = "bottom")
-ggsave("inference_type_2_amen.png", width = 12, height = 8)
+  theme(axis.text.x = element_text(angle = 90, hjust = 1), legend.position = "bottom",
+    plot.title = element_text(hjust = 0.5))
+ggsave("inference_type_2_amen.png", width = 12, height = 8, scale = .75)
